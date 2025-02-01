311b002bf2c857e1f291254ab89d46b481a49aae
arjunkumar811 <kumararjun26401@gmail.com>
2025-02-01 16:20:38 +0530

    Added Shimmer ui

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,16 +1,20 @@
 function Shimmer() {
-    return <>
-    <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-    </div>
-    </>
+    return (
+        <>
+            <div className="shimmer-container flex flex-wrap">
+                {Array.from({ length: 8 }).map((_, index) => (
+                    <div key={index} className="p-4 m-4 w-[250px] rounded-lg bg-white shadow-lg animate-pulse">
+                        <div className="h-40 bg-gray-200 rounded-lg"></div>
+                        <div className="font-bold py-4 text-lg bg-gray-200 h-6 rounded"></div>
+                        <div className="text-sm bg-gray-200 h-4 rounded mb-2"></div>
+                        <div className="text-sm bg-gray-200 h-4 rounded mb-2"></div>
+                        <div className="text-sm bg-gray-200 h-4 rounded mb-2"></div>
+                        <div className="text-sm bg-gray-200 h-4 rounded"></div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default Shimmer;
