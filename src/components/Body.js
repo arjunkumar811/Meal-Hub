a1abe6ab80cc0e1c98d54dff330f88656f12fc2a
arjunkumar811 <kumararjun26401@gmail.com>
2025-02-01 16:09:43 +0530

    Added Restro card

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,6 +23,7 @@ const fetchData = async () => {
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
   );
 
+  
 
   const json = await response.json();
 
--- src/components/Rescards.js
+++ src/components/Rescards.js
@@ -1,9 +1,7 @@
+import PropTypes from 'prop-types';
 import { CDN_URL } from "../utils/constants";
 
-const Rescards = (props) => {
-    const { resData } = props;
-  
-    // Provide a default empty object for `info` to avoid destructuring undefined
+const Rescards = ({ resData }) => {
     const {
         name = "Unknown Restaurant",
         cloudinaryImageId = "",
@@ -11,22 +9,35 @@ const Rescards = (props) => {
         avgRating = "N/A",
         deliveryTime = "N/A",
         areaName = "N/A"
-    } = resData?.info || {}; // Destructure `info` with default values
+    } = resData?.info || {};
 
     return (
-      <div className=" p-4 m-4 w-[250px] rounded-lg bg-red-500 hover:bg-gray-400">
-        <img 
-          className="card1 w-full w-29 rounded-lg"
-          src={CDN_URL + cloudinaryImageId}
-          alt="Restaurant"
-        />
-        <h3 className="font-bold py-4 text-lg">{name}</h3>
-        <h4>{cuisines.join(", ")}</h4> {/* Display cuisines as comma-separated values */}
-        <h4>Rating: {avgRating}</h4>
-        <h4>Delivery Time: {deliveryTime} mins</h4>
-        <h4>Area: {areaName}</h4>
-      </div>
+        <div className="p-4 m-4 w-[250px] rounded-lg cursor-pointer bg-white shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:translate-x-2">
+            <img 
+                className="card1 w-full rounded-lg"
+                src={CDN_URL + cloudinaryImageId}
+                alt={`Image of ${name}`}
+            />
+            <h3 className="font-bold py-4 text-lg text-gray-800">{name}</h3>
+            <h4 className="text-sm text-gray-600">{cuisines.join(", ")}</h4>
+            <h4 className="text-sm text-gray-600">Rating: {avgRating}</h4>
+            <h4 className="text-sm text-gray-600">Delivery Time: {deliveryTime} mins</h4>
+            <h4 className="text-sm text-gray-600">Area: {areaName}</h4>
+        </div>
     );
+};
+
+Rescards.propTypes = {
+    resData: PropTypes.shape({
+        info: PropTypes.shape({
+            name: PropTypes.string,
+            cloudinaryImageId: PropTypes.string,
+            cuisines: PropTypes.arrayOf(PropTypes.string),
+            avgRating: PropTypes.string,
+            deliveryTime: PropTypes.string,
+            areaName: PropTypes.string,
+        }),
+    }).isRequired,
 };
 
 export default Rescards;
